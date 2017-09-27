c77c4d18934c9c77f0ba5bb025e46e0e1b88615d
WouterBoghaert <wouter.boghaert@gmail.com>
2017-09-27 13:02:42 +0200

    hallo

---
+++ src/gitCursus/GitCursus.java
@@ -0,0 +1,10 @@
+package gitCursus;
+
+public class GitCursus {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hallo");
+    }
+    
+}
